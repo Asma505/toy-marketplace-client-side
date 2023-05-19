3500252c9dddc90088973f23e0eb66452ed79473
Asma505 <sonali.hcn@gmail.com>
2023-05-19 10:15:12 +0600

    made navbar conditional

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
 // import logonew from '../../assets/logo3.png';
 
 const Navbar = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then()
+            .catch(error => console.log(error));
+    }
 
     const menues = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to=''>All Toys</Link></li>
         <li><Link to=''>Blogs</Link></li>
-        <li><Link to=''>My Toys</Link></li>
-        <li><Link to=''>Add A Toy</Link></li>
-        <li><Link to=''>Logout</Link></li>
-        <li><Link to='/login'>Login</Link></li>        
+        {user &&
+            <><li><Link to=''>My Toys</Link></li>
+                <li><Link to=''>Add A Toy</Link></li>
+            </>}
     </>
+
+
     return (
         <div>
             <div className='bg-black py-1'>
@@ -26,7 +38,7 @@ const Navbar = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </label>
                             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {menues}
+                                {menues}
                             </ul>
                         </div>
                         <a className="normal-case text-xl font-bold">Sports Toys</a>
@@ -36,9 +48,15 @@ const Navbar = () => {
                             {menues}
                         </ul>
                     </div>
+
                     <div className="navbar-end">
-                        <a className="btn">Get started</a>
+                        {user ? <><img className='w-16 pr-3' src={user.photoURL} alt="" />
+
+                            <button className="btn"><Link onClick={handleLogOut}>Logout</Link></button></> :
+                            <button className="btn"><Link to='/login'>Login</Link></button>}
                     </div>
+
+
                 </div>
             </div>
         </div>
